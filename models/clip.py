0e6b2c4683ba5d987a69eb88a4f825b514483680
Alex Huang <whuang288@wisc.eddu>
2023-01-28 17:00:58 +0000

    sample run

---
+++ models/clip.py
@@ -0,0 +1,87 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from dataclasses import dataclass
+from typing import Tuple
+
+import torch
+import clip
+from config import BaseModelConfig, InferenceConfig
+from models.common import ThreeCrop, Mirror
+from pytorchvideo.transforms import ApplyTransformToKey, ShortSideScale
+from torch.nn import Identity, Module
+from torchvision.transforms import Compose, Lambda
+from torchvision.transforms._transforms_video import NormalizeVideo
+
+@dataclass
+class ModelConfig(BaseModelConfig):
+    model_name: str = "clip"
+    input_type: str = "video"
+    side_size: int = 224
+    crop_size: int = 224
+    dilation: int = 1
+    clip_to_image: int = 8
+    mean: Tuple[float] = (0.485, 0.456, 0.406)
+    std: Tuple[float] = (0.229, 0.224, 0.225)
+
+class WrapModel(Module):
+    def __init__(self, model: Module):
+        super().__init__()
+        self.model = model
+
+    def forward(self, x) -> torch.Tensor:
+        input = x['video']
+        if input.ndim == 5:
+            out = self.model(input[:,:,0])
+        elif input.ndim == 6:
+            # input.shape: [1(bs), 6(mirror + 3crop), 3(color), 32(T), 224(w), 224(h)]
+            # output.shape: [1(bs), 512(feature), 6(mirror + 3crop)] -> [1, 512, 2(mirror), 3(3crop)]
+            out = torch.stack([self.model(input[:, i, :, 0]) for i in range(input.shape[1])], dim = -1).view(input.shape[0], -1, 2, 3)
+        else:
+            raise ValueError('invalid input size')
+        return out
+
+def load_model(
+    inference_config: InferenceConfig,
+    config: ModelConfig,
+    patch_final_layer: bool = True,
+) -> Module:
+    model, preprocess = clip.load("ViT-B/32", device = InferenceConfig.device)
+    model = model.encode_image
+    model = WrapModel(model)
+    model = model.eval()
+    return model
+
+# class PackPathway(torch.nn.Module):
+#     """
+#     Transform for converting video frames as a list of tensors.
+#     """
+#     def __init__(self, clip_to_image):
+#         super().__init__()
+#         self.clip_to_image = clip_to_image
+
+#     def forward(self, frames: torch.Tensor):
+#         # Perform temporal sampling from the fast pathway.
+#         images = torch.index_select(
+#             frames,
+#             -3,
+#             torch.linspace(
+#                 0, frames.shape[-3] - 1, frames.shape[-3] // self.clip_to_image
+#             ).long(),
+#         )
+#         return images
+    
+def get_transform(inference_config: InferenceConfig, config: ModelConfig):
+    assert config.input_type == "video"
+    transforms = [
+        Lambda(lambda x: x[:, ::config.dilation]),
+        Lambda(lambda x: x / 255.0),
+        NormalizeVideo(config.mean, config.std),
+        ShortSideScale(size=config.side_size),
+        ThreeCrop(config.crop_size),
+        Mirror(),
+    ]
+    
+    return ApplyTransformToKey(
+        key="video",
+        transform=Compose(transforms),
+    )
---
+++ models/slowfast.py
@@ -0,0 +1,106 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+import torch
+from torch import nn
+from config import BaseModelConfig, InferenceConfig
+from models.common import FeedVideoInputList, Mirror
+from pytorchvideo.transforms import ApplyTransformToKey, ShortSideScale
+from torch.nn import Identity, Module
+from torchvision.transforms import Compose, Lambda
+from torchvision.transforms._transforms_video import NormalizeVideo
+
+
+@dataclass
+class ModelConfig(BaseModelConfig):
+    model_path: Optional[str] = None
+    hub_path: Optional[str] = "slowfast_r101"
+    slowfast_alpha: int = 4
+
+    # transformation config
+    side_size: int = 288    # (512, 288) for 16:9 videos, (384, 288) for 4:3 videos
+    dilation: int = 2       # slowfast takes every other frame from 64-frame clips
+    mean: Tuple[float] = (0.45, 0.45, 0.45)
+    std: Tuple[float] = (0.225, 0.225, 0.225)
+
+
+class GetFv(Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        bs = x.shape[0]
+        fv_s = x.shape[1]
+        return x.view(bs, fv_s)
+
+
+def load_model(
+    inference_config: InferenceConfig,
+    config: ModelConfig,
+    patch_final_layer: bool = True,
+) -> Module:
+    if config.model_path is not None:
+        raise AssertionError("not supported yet")
+        model = None
+    else:
+        assert config.hub_path is not None
+        model = torch.hub.load(
+            "facebookresearch/pytorchvideo", config.hub_path, pretrained=True
+        )
+
+    assert model is not None
+
+    # replace fixed-sized pooling with adaptive pooling to allow 
+    # flexible input size (original implementation assumes 256x256 input)
+    if patch_final_layer:
+        model.blocks[5].pool = nn.ModuleList(
+            [nn.AdaptiveAvgPool3d(1), nn.AdaptiveAvgPool3d(1)]
+        )
+        model.blocks[6] = GetFv()
+
+    # Set to GPU or CPU
+    model = FeedVideoInputList(model)
+    model = model.eval()
+    model = model.to(inference_config.device)
+    return model
+
+
+class PackPathway(torch.nn.Module):
+    """
+    Transform for converting video frames as a list of tensors.
+    """
+
+    def __init__(self, slow_fast_alpha):
+
+        super().__init__()
+        self.slow_fast_alpha = slow_fast_alpha
+
+    def forward(self, frames: torch.Tensor):
+        fast_pathway = frames
+        # Perform temporal sampling from the fast pathway.
+        slow_pathway = torch.index_select(
+            frames,
+            -3,
+            torch.linspace(
+                0, frames.shape[-3] - 1, frames.shape[-3] // self.slow_fast_alpha
+            ).long(),
+        )
+        frame_list = [slow_pathway, fast_pathway]
+        return frame_list
+
+def get_transform(inference_config: InferenceConfig, config: ModelConfig):
+    return ApplyTransformToKey(
+        key="video",
+        transform=Compose(
+            [
+                Lambda(lambda x: x[:, ::config.dilation]),
+                Lambda(lambda x: x / 255.0),
+                NormalizeVideo(config.mean, config.std),
+                ShortSideScale(size=config.side_size),
+                Mirror(),   # include horizontal flip
+                PackPathway(config.slowfast_alpha),
+            ]
+        ),
+    )
