8f665d8800e89a017614b02a190f62a0bb4cddd2
Alex Huang <whuang288@wisc.edu>
2023-02-06 21:06:17 -0600

    remove debug output

--- models/clip.py
+++ models/clip.py
@@ -31,12 +31,10 @@ class WrapModel(Module):
     def forward(self, x) -> torch.Tensor:
         input = x['video']
         if input.ndim == 5:
-            print("input:", input.shape)
             out = self.model(input[:,:,0])
         elif input.ndim == 6:
             # input.shape: [1(bs), 6(mirror + 3crop), 3(color), 8(T), 224(w), 224(h)]
             # output.shape: [1(bs), 512(feature), 6(mirror + 3crop)] -> [1, 512, 2(mirror), 3(3crop)]
-            print("input:", input.shape)
             out = torch.stack([self.model(input[:, i, :, 0]) for i in range(input.shape[1])], dim = -1).view(input.shape[0], -1, 2, 3)
         else:
             raise ValueError('invalid input size')
