be4bd62e3b73c6cdc948109b4d3e23a1cc23aa9c
Alex Huang <whuang288@wisc.eddu>
2023-02-15 17:48:55 +0000

    started adding code for c3d

---
+++ models/c3d.py
@@ -0,0 +1,69 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates. All Rights Reserved.
+
+from dataclasses import dataclass
+from typing import Tuple
+
+import torch
+from config import BaseModelConfig, InferenceConfig
+from models.common import FeedVideoInput, ThreeCrop, Mirror
+from pytorchvideo.transforms import ApplyTransformToKey, ShortSideScale
+from torch.nn import Identity, Module
+from torchvision.transforms import Compose, Lambda
+from torchvision.transforms._transforms_video import  CenterCropVideo, NormalizeVideo
+
+
+@dataclass
+class ModelConfig(BaseModelConfig):
+    pretrained_dataset: str = "sports1m"
+
+    # transformation config
+    side_size: int = 288
+    dilation: int = 1
+    mean: Tuple[float] = (0.5, 0.5, 0.5)
+    std: Tuple[float] = (0.5, 0.5, 0.5)
+
+
+def load_model(
+    inference_config: InferenceConfig,
+    config: ModelConfig,
+    patch_final_layer: bool = True,
+) -> Module:
+    print("Loading I3D")    # takes RGB / XY input
+    model = build_i3d(pretrained=config.pretrained_dataset)
+
+    assert model is not None
+
+    if patch_final_layer:
+        model.logits = Identity()
+
+    # Set to GPU or CPU
+    model = FeedVideoInput(model)
+    model = model.eval()
+    model = model.to(inference_config.device)
+    return model
+
+
+def get_transform(inference_config: InferenceConfig, config: ModelConfig):
+    transforms = [
+        Lambda(lambda x: x[:, ::config.dilation]),
+        Lambda(lambda x: x / 255.0),
+        NormalizeVideo(config.mean, config.std),
+        ShortSideScale(size=config.side_size),
+    ]
+    
+    if config.center_crop:
+        transforms.append(CenterCropVideo(config.crop_size))
+    elif config.three_crop:
+        transforms.append(ThreeCrop(config.crop_size))
+
+    if config.mirror:
+        transforms.append(Mirror())
+        
+    return Compose(
+        [
+            ApplyTransformToKey(
+                key="video",
+                transform=Compose(transforms),
+            )
+        ]
+    )
